ae685959d31f5325e6a95b0b4b985bc8027da3f8
Tatsuya Sasaki <tasasaki@gmail.com>
2016-05-11 16:33:57 +0900

    Add the control point for the notification period
    and fix some miss spellings, inconsistent indentations.
    The period variable seems to be in millisecond though Ref.1(*) and Ref.2(**) state it as unit of 10 millisecond.
    (*) http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Accelerometer
    (**) http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User's_Guide#Movement_Sensor

--- sensorTag/sensorTagExample/sensortagAccelerometer.js
+++ sensorTag/sensorTagExample/sensortagAccelerometer.js
@@ -9,9 +9,10 @@
 	read a tag:
 		1) discover the tag
 		2) connect to and set up the tag
-		3) turn on the sensor you want to use (in this case, accelerometer)
-		4) turn on notifications for the sensor
-		5) listen for changes from the sensortag
+		3) set the notification period
+		4) turn on the sensor you want to use (in this case, accelerometer)
+		5) turn on notifications for the sensor
+		6) listen for changes from the sensortag
 
 	This example does all of those steps in sequence by having each function
 	call the next as a callback. Discover calls connectAndSetUp, and so forth.
@@ -21,60 +22,66 @@
 
 	created 15 Jan 2014
 	by Tom Igoe
-*/
+ */
 
-
-var SensorTag = require('sensortag');		// sensortag library
+var SensorTag = require('sensortag'); // sensortag library
 
 // listen for tags:
 SensorTag.discover(function(tag) {
-	// when you disconnect from a tag, exit the program:
-	tag.on('disconnect', function() {
-		console.log('disconnected!');
-		process.exit(0);
-	});
+  // when you disconnect from a tag, exit the program:
+  tag.on('disconnect', function() {
+    console.log('disconnected!');
+    process.exit(0);
+  });
 
-	function connectAndSetUpMe() {			// attempt to connect to the tag
-     console.log('connectAndSetUp');
-     tag.connectAndSetUp(enableAccelMe);		// when you connect and device is setup, call enableAccelMe
-   }
+  function connectAndSetUpMe() { // attempt to connect to the tag
+    console.log('connectAndSetUp');
+    // when you connect and device is setup, call setPeriod
+    tag.connectAndSetUp(setPeriod);
+  }
 
-   function enableAccelMe() {		// attempt to enable the accelerometer
-     console.log('enableAccelerometer');
-     // when you enable the accelerometer, start accelerometer notifications:
-     tag.enableAccelerometer(notifyMe);
-   }
+  function setPeriod() { // attempt to set the notification period
+    console.log('setPeriod');
+    // when you set the period, call enableAccelMe
+    tag.setAccelerometerPeriod(1000, enableAccelMe);
+  }
 
-	function notifyMe() {
-   	tag.notifyAccelerometer(listenForAcc);   	// start the accelerometer listener
-		tag.notifySimpleKey(listenForButton);		// start the button listener
-   }
+  function enableAccelMe() { // attempt to enable the accelerometer
+    console.log('enableAccelerometer');
+    // when you enable the accelerometer, start accelerometer notifications:
+    tag.enableAccelerometer(notifyMe);
+  }
 
-   // When you get an accelermeter change, print it out:
-	function listenForAcc() {
-		tag.on('accelerometerChange', function(x, y, z) {
-	     console.log('\tx = %d G', x.toFixed(1));
-	     console.log('\ty = %d G', y.toFixed(1));
-	     console.log('\tz = %d G', z.toFixed(1));
-	   });
-	}
+  function notifyMe() {
+    tag.notifyAccelerometer(listenForAcc); // start the accelerometer listener
+    tag.notifySimpleKey(listenForButton); // start the button listener
+  }
 
-	// when you get a button change, print it out:
-	function listenForButton() {
-		tag.on('simpleKeyChange', function(left, right) {
-			if (left) {
-				console.log('left: ' + left);
-			}
-			if (right) {
-				console.log('right: ' + right);
-			}
-			// if both buttons are pressed, disconnect:
-			if (left && right) {
-				tag.disconnect();
-			}
-	   });
-	}
+  // When you get an accelerometer change, print it out:
+  function listenForAcc() {
+    tag.on('accelerometerChange', function(x, y, z) {
+      console.log('\tx = %d G', x.toFixed(1));
+      console.log('\ty = %d G', y.toFixed(1));
+      console.log('\tz = %d G', z.toFixed(1));
+    });
+  }
 
-	// Now that you've defined all the functions, start the process:
-	connectAndSetUpMe();
+  // when you get a button change, print it out:
+  function listenForButton() {
+    tag.on('simpleKeyChange', function(left, right) {
+      if (left) {
+        console.log('left: ' + left);
+      }
+      if (right) {
+        console.log('right: ' + right);
+      }
+      // if both buttons are pressed, disconnect:
+      if (left && right) {
+        tag.disconnect();
+      }
+    });
+  }
+
+  // Now that you've defined all the functions, start the process:
+  connectAndSetUpMe();
 });
